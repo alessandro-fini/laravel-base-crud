60880e5996817a67b9a5544572b739a6c575e486
alessandro <finialessandro90@gmail.com>
2021-03-17 14:27:39 +0100

    pagina home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <div class="container">
+        <h1>pagina home</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis veniam ea molestiae, ducimus incidunt unde odio, eaque deserunt aperiam quos et itaque omnis aliquid, aut sapiente nihil consectetur consequuntur possimus.</p>
+    </div>
+@endsection
